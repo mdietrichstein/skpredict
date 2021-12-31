ab4320d4097cbc1e12619c42f518dad6747d4d1c
Marc Dietrichstein <marc@hero-workout.com>
2021-12-31 22:10:00 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="skpredict",
-    install_requires={
+    install_requires=[
         "numpy>=1.20"
-    },
+    ],
     extras_require={
         "scikit-learn": ["scikit-learn>=0.22.0"]
     }
